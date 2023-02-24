28f80eff40308a8b343f71cd16549f67a2b270d7
stuartore <stuart.dr@qq.com>
2023-02-24 10:17:39 +0800

    kernel: update branch to void aosp-13 & use defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,12 +130,13 @@ BOARD_RAMDISK_USE_LZ4 := true
 KERNEL_LLVM_SUPPORT := true
 SELINUX_IGNORE_NEVERALLOWS := true
 
-TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig vendor/xiaomi/sm8250-common.config
-#TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig vendor/xiaomi/xiaomi-kona-common.config
-TARGET_KERNEL_CONFIG += vendor/xiaomi/psyche.config
+#TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig vendor/xiaomi/sm8250-common.config
+#TARGET_KERNEL_CONFIG += vendor/xiaomi/psyche.config
+
+TARGET_KERNEL_CONFIG := psyche_defconfig
 
 #TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8250
-TARGET_KERNEL_SOURCE := kernel/xiaomi/void-sm8250
+TARGET_KERNEL_SOURCE := kernel/xiaomi/void-aosp-sm8250
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
