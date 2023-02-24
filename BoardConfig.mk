1de74c082314e3bd0385212f5806ded404fdab9b
stuartore <stuart.dr@qq.com>
2023-02-24 15:35:30 +0800

    psyche: adjust FCM for Crdroid bringup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,7 +95,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/xiaomi:libudfps_extension.xiaomi
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/hidl/device_framework_matrix.xml \
     $(DEVICE_PATH)/configs/hidl/xiaomi_framework_compatibility_matrix.xml \
-    vendor/aosp/config/device_framework_matrix.xml
+    vendor/lineage/config/device_framework_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
      $(DEVICE_PATH)/dolby/manifests/dolby_framework_matrix.xml
